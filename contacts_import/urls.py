aa8e71a5b395d654e70a8befe89a902436db8d17
Guillaume Libersat <guillaume@spreadband.com>
2010-11-10 15:10:24 +0100

    use module attributes, not strings

--- contacts_import/urls.py
+++ contacts_import/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 
+import views
 
-urlpatterns = patterns("",
-    url(r"^import_contacts/$", "contacts_import.views.import_contacts", name="import_contacts"),
-    url(r"^import_google_contacts/$", "contacts_import.views.import_google_contacts", name="import_google_contacts"),
-    url(r"^select_contacts/$", "contacts_import.views.select_contacts", name="select_contacts"),
-                       #url(r"^authsub/login/$", "contacts_import.views.authsub_login", name="authsub_login"),
+urlpatterns = patterns('',
+    url(r'^import_contacts/$', views.import_contacts, name='import_contacts'),
+
+    # Backends
+    url(r'^import_google_contacts/$', views.import_google_contacts, name='import_google_contacts'),
+    url(r'^import_email_list/$', views.import_email_list, name='import_email_list'),
+
+    # Contact selection
+    url(r'^select_contacts/$', views.select_contacts, name='select_contacts'),
 )
